13af3937467493cdbacb34e8bb0cd75e47a3e3b5
Mārtiņš Avots <martins.avots@splitcells.net>
2025-02-01 05:50:16 +0100

    Move all system config into cell #42
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- src/main/java/net/splitcells/network/presentations/NetworkPresentationsFileSystem.java
+++ src/main/java/net/splitcells/network/presentations/NetworkPresentationsFileSystem.java
@@ -15,12 +15,12 @@
  */
 package net.splitcells.network.presentations;
 
-import net.splitcells.dem.environment.config.framework.OptionI;
+import net.splitcells.dem.environment.config.framework.OptionImpl;
 import net.splitcells.dem.resource.FileSystemView;
 
 import static net.splitcells.dem.resource.FileSystemViaClassResources.fileSystemViaClassResources;
 
-public class NetworkPresentationsFileSystem extends OptionI<FileSystemView> {
+public class NetworkPresentationsFileSystem extends OptionImpl<FileSystemView> {
     public NetworkPresentationsFileSystem() {
         super(() -> fileSystemViaClassResources(NetworkPresentationsFileSystem.class
                 , "net.splitcells", "network.presentations"));
